cfe023c4e35fd1256f950d6c034118e3b2a6ece8
Nathan Alex <nate745@gmail.com>
2019-08-08 23:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({prop:1}, {prop2:2})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  return object.key = "value"
+}
