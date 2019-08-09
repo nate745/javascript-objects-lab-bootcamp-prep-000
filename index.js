c4abf329f59d55a06ac18c7ac0eab686386ea430
Nathan Alex <nate745@gmail.com>
2019-08-09 00:00:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(object,key,value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
-  return object[`prop2`] = 2
+  return object[`prop2`] = `2`
 }
