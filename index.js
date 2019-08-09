4995a8d1dd66ac5f6e67e746aedee2dbec0b42fc
Nathan Alex <nate745@gmail.com>
2019-08-09 00:17:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 function deleteFromObjectByKey(object,key){
   var newObject = Object.assign({},object)
   delete newObject.prop
-  return object
+  return newObject
 }
